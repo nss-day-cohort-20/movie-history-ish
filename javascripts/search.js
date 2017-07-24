a8e7e9495159b9cefda36eebc084daa828307766
Joe Shepherd <joeshepmedia@gmail.com>
2017-07-24 01:18:54 -0500

    Filter duplicate movies on search; show faves w/filter btn clicked

--- javascripts/search.js
+++ javascripts/search.js
@@ -3,6 +3,11 @@
 let apiCtrl = require("./api-controller");
 let movieCtrl = require('./movie-ctrl');
 let templates = require('./template-builder');
+// https://lodash.com/docs/4.17.4#uniqBy Remember lodash? Comes in handy a lot.
+// This method allows us to remove duplicate objects from an array based on a shared property
+let _uniqBy = require('lodash/uniqBy');
+
+// console.log("_uniqBy", _uniqBy);
 
 $('#search-btn').click(function() {
   console.log("search movies clicked");
@@ -25,13 +30,21 @@ $('.filter-btn').click( function() {
   event.preventDefault();
   // match the button's name attr with the classes on the movie cards
   let cardClass = `.${$(this).attr("title")}`;
+  console.log("favorite?", cardClass);
   $(".movie-list__item").hide();
-  $(cardClass).show();
+  if(cardClass === ".favorite") {
+    console.log("fave clicked");
+    console.log("fave?", $(".watched").find("div[data-rating='10']"));
+    $(".watched").has("div[data-rating='10']").show();
+  } else {
+    $(cardClass).show();
+  }
 });
 
 function outputMovies(moviesData) {
   console.log("allMovies", moviesData );
-  let movieCards = templates.makeMovieList(moviesData);
+  let uniqueMovies = filterDuplicates(moviesData);
+  let movieCards = templates.makeMovieList(uniqueMovies);
   $(".movie-list").html(movieCards);
   $(".rating").starRating({
     minus: true // step minus button
@@ -44,4 +57,8 @@ function outputMovies(moviesData) {
     let $stars = ($(this).find("li"));
     $stars.slice(0, ratingNum).addClass("active");
   });
+}
+
+function filterDuplicates(movies) {
+  return _uniqBy(movies, 'poster_path');
 }
